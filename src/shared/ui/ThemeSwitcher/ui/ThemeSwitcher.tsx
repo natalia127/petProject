c32e04a8aaabfea710837b260bbdc0cbfbc2f5c2
natalia <ndorogan@bk.ru>
2024-02-07 15:23:39 +0300

    add main pipeline actions

--- src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/shared/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -2,8 +2,7 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import LightIcon from 'shared/assets/icons/theme-light.svg';
 import DarkIcon from 'shared/assets/icons/theme-dark.svg';
 import { Button, ThemeButton } from 'shared/ui/Button/Button';
-import { Theme, useTheme } from 'app/provider/ThemeProvider';
-import cls from './ThemeSwitcher.module.scss';
+import { useTheme } from 'app/provider/ThemeProvider';
 
 interface ThemeSwitcherProps {
   className?: string;
@@ -15,7 +14,7 @@ export function ThemeSwitcher({ className }: ThemeSwitcherProps) {
     return (
         <Button
             theme={ThemeButton.CLEAR}
-            className={classNames(cls.ThemeSwitcher, {}, [className])}
+            className={classNames('', {}, [className])}
             onClick={toggleTheme}
         >
             {theme === 'dark' ? <DarkIcon /> : <LightIcon />}
